f4eaaecc4976dfd92034e335cf578f1ea5277837
Nicole Valletta <nicolelouisev@gmail.com>
2023-12-13 16:32:40 -0300

    Inicializando projeto com teste de funcionamento correto das classes

---
+++ src/conta/Menu.java
@@ -0,0 +1,13 @@
+package conta;
+
+import conta.util.Cores;
+
+public class Menu {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Teste de importação correta da classe Cor: " + Cores.testarCores());
+
+	}
+
+}
---
+++ src/conta/util/Cores.java
@@ -0,0 +1,9 @@
+package conta.util;
+
+public class Cores {
+	
+	public static String testarCores() {
+		String cor = "cor";
+		return cor;
+	}
+}
