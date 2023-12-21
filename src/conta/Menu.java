44c7f278a7c29723b2b3988d80d04c978df2f66c
Nicole Valletta <nicolelouisev@gmail.com>
2023-12-20 22:18:16 -0300

    Implementado os métodos sacar, depositar e transferir

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -15,9 +15,9 @@ public class Menu {
 		
 		Scanner leia = new Scanner(System.in);
 		
-		int opcao, numero, agencia, tipo, aniversario;
+		int opcao, numero, agencia, tipo, aniversario, numeroDestino;
 		String titular;
-		float saldo, limite;
+		float saldo, limite, valor;
 		
 		ContaController contas = new ContaController();
 		
@@ -175,15 +175,47 @@ public class Menu {
 				case 6:
 					System.out.println(Cores.TEXT_WHITE_BOLD + "Saque\n\n");
 					
+					System.out.println("Digite o número da conta: ");
+					numero = leia.nextInt();
+					
+					do {
+						System.out.println("Digite o valor do saque (R$): ");
+						valor = leia.nextFloat();
+					} while (valor <= 0);
+					
+					contas.sacar(numero, valor);
+					
 					keyPress();
 					break;
 				case 7:
 					System.out.println(Cores.TEXT_WHITE_BOLD + "Depósito\n\n");
 					
+					System.out.println("Digite o número da conta: ");
+					numero = leia.nextInt();
+					
+					do {
+						System.out.println("Digite o valor do depósito (R$): ");
+						valor = leia.nextFloat();
+					} while (valor <= 0);
+					
+					contas.depositar(numero,  valor);
+					
 					keyPress();
 					break;
 				case 8:
 					System.out.println(Cores.TEXT_WHITE_BOLD + "Transferência entre contas\n\n");
+					
+					System.out.println("Digite o número da conta de origem: ");
+					numero = leia.nextInt();
+					System.out.println("Digite o número da conta de destino: ");
+					numeroDestino = leia.nextInt();
+					
+					do {
+						System.out.println("Digite o valor da transferência (R$): ");
+						valor = leia.nextFloat();
+					} while (valor <= 0);
+					
+					contas.transferir(numero, numeroDestino, valor);
 					
 					keyPress();
 					break;
